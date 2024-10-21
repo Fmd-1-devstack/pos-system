af1e53dadabb1c1ccc164997d68d564f05797016
education-bootcamp <education.seekerscloud@gmail.com>
2024-10-21 20:40:54 +0530

    init

--- app.js
+++ app.js
@@ -2,3 +2,38 @@
 document.getElementById('customerContext').style.display='block';
 document.getElementById('ordersContext').style.display='none';
 document.getElementById('productsContext').style.display='none';
+
+//========== manage tabs===============
+
+document.getElementById('customerTab').addEventListener('click',function(event){
+  event.preventDefault();
+  document.getElementById('customerContext').style.display='block';
+  document.getElementById('ordersContext').style.display='none';
+  document.getElementById('productsContext').style.display='none';
+
+  this.classList.add('active');
+  document.getElementById('productsTab').classList.remove('active');
+  document.getElementById('ordersTab').classList.remove('active');
+});
+
+document.getElementById('productsTab').addEventListener('click',function(event){
+  event.preventDefault();
+  document.getElementById('customerContext').style.display='none';
+  document.getElementById('ordersContext').style.display='none';
+  document.getElementById('productsContext').style.display='block';
+
+  this.classList.add('active');
+  document.getElementById('customerTab').classList.remove('active');
+  document.getElementById('ordersTab').classList.remove('active');
+});
+
+document.getElementById('ordersTab').addEventListener('click',function(event){
+  event.preventDefault();
+  document.getElementById('customerContext').style.display='none';
+  document.getElementById('ordersContext').style.display='block';
+  document.getElementById('productsContext').style.display='none';
+
+  this.classList.add('active');
+  document.getElementById('productsTab').classList.remove('active');
+  document.getElementById('productsTab').classList.remove('active');
+});
