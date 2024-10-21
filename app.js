07e527264ad6695c0d4c43655e1add013354d8ba
education-bootcamp <education.seekerscloud@gmail.com>
2024-10-21 20:31:33 +0530

    init

---
+++ app.js
@@ -0,0 +1,4 @@
+// hide all the tab contexts
+document.getElementById('customerContext').style.display='block';
+document.getElementById('ordersContext').style.display='none';
+document.getElementById('productsContext').style.display='none';
