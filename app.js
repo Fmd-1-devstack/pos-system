575bb8b5615d3c7e59ead8996e196ebdb2e48017
education-bootcamp <education.seekerscloud@gmail.com>
2024-10-23 20:27:19 +0530

    init

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+        import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js';
+        import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-analytics.js';
+        import { getAuth,createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, signOut } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js';
+        import { getFirestore, doc, setDoc } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js';
+        import { getStorage, ref, uploadBytes, getDownloadURL } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-storage.js';
+
+        const firebaseConfig = {
+          apiKey: "AIzaSyCK9tcze0I01uSWeNayqUmdY490MiFEVmw",
+          authDomain: "pos-system-c06a5.firebaseapp.com",
+          projectId: "pos-system-c06a5",
+          storageBucket: "pos-system-c06a5.appspot.com",
+          messagingSenderId: "509587287340",
+          appId: "1:509587287340:web:85353adb426ae469701089",
+          measurementId: "G-RD556ECTDR"
+      };
+      const app = initializeApp(firebaseConfig);
+      const auth = getAuth(app);
+      const db = getFirestore(app);
+      const storage = getStorage(app);
+
 
 // hide all the tab contexts
 document.getElementById('customerContext').style.display='block';
@@ -44,7 +64,7 @@ document.getElementById('signupButton').addEventListener('click', function(event
   document.getElementById('errorRegisterAlert').style.display='none';
 });
 
-function register(){
+const register=()=>{
   let email = document.getElementById('registerUsername').value;
   let password = document.getElementById('registerPassword').value;
 
@@ -53,12 +73,15 @@ function register(){
     return;
   }
 
-  auth.createUserWithEmailAndPassword(email, password)
+  createUserWithEmailAndPassword(email, password)
   .then((userCredentials)=>{
     console.log(userCredentials);
   }).catch((error)=>{
     console.log(error);
   })
 }
+
+
+window.register=register;
 
 //============Register with email & password
